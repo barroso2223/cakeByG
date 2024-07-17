5df1659ef671145de725936383f728affce04b0b
CyBarrD <barroso8292@gmail.com>
2024-07-17 16:53:44 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// const scrollers = document.querySelectorAll(".slider");
+
+// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+//     addAnimation();
+// }
+
+// function addAnimation() {
+//     scrollers.forEach((scroller) => {
+//         scroller.setAttribute("data-animated", true)
+//     })
+// }
